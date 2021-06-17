8bc006bac57ce046be6518cc37c7605b57c5c404
Wuodabudo <omendajoe@gmail.com>
2021-06-17 13:04:20 -0700

    modified:   0x00-hello_world/4-puts.c 	new file:   0x00-hello_world/5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-#include <string.h>                                                             int main(void)
 {
-char string[];
-strcpy(str, "Programming is like building a multilingual puzzle/n");
-puts(string);
-return (0);
+char str[] = "Programming is like building a multilingual puzzle";
+puts(str);
 }
