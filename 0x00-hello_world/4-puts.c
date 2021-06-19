5d9b73927ec2ccaaf5e7f9e2c8a7043625ddb8b3
Wuodabudo <omendajoe@gmail.com>
2021-06-19 15:02:16 -0700

    modified:   0x00-hello_world/.5-printf.c.swp 	new file:   0x00-hello_world/100-intel 	new file:   0x00-hello_world/101-quote.c 	modified:   0x00-hello_world/4-puts.c 	modified:   0x00-hello_world/5-printf.c 	new file:   0x00-hello_world/6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-main()
+/**
+ * main - entry
+ *
+ * Return:0
+ * Description - c
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+};
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-int main(void)
 /**
- * function_name - Short description, single line
- * @parameterx: Description of parameter x
-(* a blank line
- * Description: Longer description of the function)?
-(* section header: Section description)*
- * Return: Description of the returned value
+ * main - entry
+ *
+ * Return: 0
  */
-{	
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
