27399714691379a53ad99437bea22987e19f8798
Wuodabudo <omendajoe@gmail.com>
2021-07-19 12:56:38 -0700

    new file:   0x0D-preprocessor/0-main.c 	new file:   0x0D-preprocessor/0-object_like_macro.h 	new file:   0x0D-preprocessor/1-main.c 	new file:   0x0D-preprocessor/1-pi.h 	new file:   0x0D-preprocessor/2-main.c 	new file:   0x0D-preprocessor/3-function_like_macro.h 	new file:   0x0D-preprocessor/3-main.c 	new file:   0x0D-preprocessor/4-main.c 	new file:   0x0D-preprocessor/4-sum.h 	new file:   0x0D-preprocessor/README.md 	new file:   0x0D-preprocessor/a 	new file:   0x0D-preprocessor/b 	new file:   0x0D-preprocessor/d 	new file:   0x0D-preprocessor/e

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include 2_MAIN
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    float a;
+    float r;
+
+    r = 98;
+    a = PI * r * r;
+    printf(");
+    return (0);
+}
