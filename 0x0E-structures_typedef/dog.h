fa74de10998e5469cb7380d1d7b02217c77e7eab
Wuodabudo <omendajoe@gmail.com>
2021-07-20 12:41:22 -0700

    modified:   0x0D-preprocessor/d 	new file:   0x0E-structures_typedef/dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ *struct dog - function
+ *@name: character pointer
+ *@owner: character pointer
+ *@age: float
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} my_dog;
+#endif
